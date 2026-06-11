10a686954353225f34100ac4ddc9229a79930efc
Amirmahdi Soheyli <amirmahdisoheyli@gmail.com>
2026-06-12 00:28:15 +0330

    feat: integrate roll dice button with game engine and update dice image views

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,10 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+import logic.engine.GameEngine;
+import ui.controller.GameBoardController;
+
+import logic.engine.GameEngine;
 
 import java.util.Objects;
 
@@ -14,10 +18,24 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        BorderPane root = FXMLLoader.load(Objects.requireNonNull(this.getClass().getResource("ui/view/GameBoard.fxml")));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("ui/view/GameBoard.fxml"));
+        BorderPane root = loader.load();
+
+        GameBoardController controller = loader.getController();
+
+        logic.engine.Map gameMap = new logic.engine.Map(5, 5);
+        java.util.List<logic.models.Player> players = new java.util.ArrayList<>();
+
+        players.add(new logic.models.TechGuruPlayer(new java.util.ArrayList<>()));
+        players.add(new logic.models.HackerCEOPlayer(new java.util.ArrayList<>()));
+
+        logic.engine.GameEngine gameEngine = new logic.engine.GameEngine(gameMap, players);
+
+        // پاس دادن موتور بازی به کنترلر
+        controller.setGameEngine(gameEngine);
 
         primaryStage.setScene(new Scene(root));
-        primaryStage.setTitle("Game Board");
+        primaryStage.setTitle("Silicon Valley: The Tech Cartel");
         primaryStage.setResizable(false);
 //        primaryStage.setFullScreen(true);
 
