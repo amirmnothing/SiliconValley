0bcdcaa74c8c116841552875ef379e96d0df8c11
Amirmahdi Soheyli <amirmahdisoheyli@gmail.com>
2026-07-05 19:11:57 +0330

    feat: add MainMenu UI and controller.

--- src/ui/controller/GameBoardController.java
+++ src/ui/controller/GameBoardController.java
@@ -7,6 +7,7 @@ import javafx.scene.Group;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.effect.ColorAdjust;
 import javafx.scene.image.Image;
